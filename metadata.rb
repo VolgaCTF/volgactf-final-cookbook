04cfa9c37593f9e394b07c0976d27f7b149f2440
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-06-07 22:02:41 +0400

    Add proxy resource

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
 name 'volgactf-final'
 description 'Installs and configures VolgaCTF Final'
-version '1.1.3'
+version '1.2.0'
 
 depends 'htpasswd', '~> 0.3.0'
 depends 'agit', '~> 0.1.2'
 depends 'ruby_rbenv', '~> 2.1.2'
 depends 'yarn', '~> 0.4.2'
 depends 'ngx', '~> 2.1.1'
+depends 'tls', '~> 3.2.0'
 
 gem 'htauth'
